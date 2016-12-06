07dc6471f0a55a77ec680c69429f58856c352310
github username <github email address>
2016-12-06 06:57:00 -0500

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require 'pry'
 class Triangle
-  # write code here
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def invalid
+    if @a <= 0 || @b <= 0 || @c <= 0
+      raise TriangleError
+    elsif @a + @b <= @c || @a+@c <= @b || @b + @c <= @a
+      raise TriangleError  
+    end
+  end
+
+  def kind
+    invalid
+    if @a == @b && @b == @c
+      :equilateral
+
+    elsif @a == @b || @b == @c || @a == @c
+      :isosceles
+
+    else 
+     :scalene   
+    end
+
+  end
+end
+
+class TriangleError < StandardError
+ 
 end
